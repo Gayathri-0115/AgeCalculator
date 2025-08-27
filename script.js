66167295f16426369b08e541a5921b662f17fbf0
Gayathri-0115 <gayathri0162005@gmail.com>
2025-08-27 18:32:24 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,48 @@
+let userInput = document.getElementById("date");
+userInput.max = new Date().toISOString().split("T")[0];
+
+function calculateAge() {
+    let birthDate = new Date(userInput.value);
+
+    if (userInput.value.trim() === ""){
+        document.getElementById("res").innerHTML = "Please Enter or Choose your BirthDate";
+        return;
+    }
+    
+    let y1 = birthDate.getFullYear();
+    let m1 = birthDate.getMonth() + 1;
+    let d1 = birthDate.getDate();
+
+    let today = new Date();
+
+    let y2 = today.getFullYear();
+    let m2 = today.getMonth() + 1;
+    let d2 = today.getDate();
+
+    let d3, m3, y3;
+
+    y3 = y2 - y1;
+
+    if (m2 >= m1) {
+        m3 = m2 - m1;
+    }
+    else {
+        y3--;
+        m3 = 12 + m2 - m1;
+    }
+    if (d2 >= d1) {
+        d3 = d2 - d1;
+    }
+    else {
+        m3--;
+        d3 = getDaysInMonth(y1, m1) + d2 - d1;
+    }
+    if (m3 < 0) {
+        m3 = 11;
+        y3--;
+    }
+    document.getElementById("res").innerHTML = `You are ${y3} Years, ${m3} Months, ${d3} Days Old.`
+}
+function getDaysInMonth(year, month) {
+    return new Date(year, month, 0).getDate();
+}
